affdfabe510f5bc0ad86a0b318f0de624f751ab9
JakobHuemer <j.huemer-fistelberger@students.htl-leonding.ac.at>
2022-12-31 00:52:56 +0100

    Tweaks with regex pattern

--- script/mail.js
+++ script/mail.js
@@ -1,37 +1,37 @@
-function sendMail() {
-  if (
-    document.getElementById('name').value == '' ||
-    document.getElementById('email').value == '' ||
-    document.getElementById('message').value == ''
-  ) {
-  }
+// function sendMail() {
+//   if (
+//     document.getElementById('name').value == '' ||
+//     document.getElementById('email').value == '' ||
+//     document.getElementById('message').value == ''
+//   ) {
+//   }
 
-  alert(document.getElementById('email').value);
+//   alert(document.getElementById('email').value);
 
-  var params = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
-  };
+//   var params = {
+//     name: document.getElementById('name').value,
+//     email: document.getElementById('email').value,
+//     message: document.getElementById('message').value,
+//   };
 
-  console.log(params);
-  alert(params);
+//   console.log(params);
+//   alert(params);
 
-  const serviceID = 'service_w9sem2z';
-  const templateID = 'template_ih18w3o';
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      // document.getElementById('name').value = '';
-      // document.getElementById('email').value = '';
-      // document.getElementById('message').value = '';
-      alert('Message sent successfully!');
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-      alert('Message failed to send.');
-    });
+//   const serviceID = 'service_w9sem2z';
+//   const templateID = 'template_ih18w3o';
+//   emailjs
+//     .send(serviceID, templateID, params)
+//     .then((res) => {
+//       // document.getElementById('name').value = '';
+//       // document.getElementById('email').value = '';
+//       // document.getElementById('message').value = '';
+//       alert('Message sent successfully!');
+//       console.log(res);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       alert('Message failed to send.');
+//     });
 
-  return;
-}
+//   return;
+// }
